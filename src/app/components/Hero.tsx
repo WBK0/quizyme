3407165750d7358090e9e4036c9a1fabea971d35
Bartlomiej <worldofworld0@gmail.com>
2023-10-19 08:25:09 +0200

    fix: Delete br tag from hero component

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -8,14 +8,14 @@ const Hero = () => {
     <div className="flex justify-between flex-wrap md:px-24 px-3 gap-y-5 gap-x-12">
       <div className="w-full max-w-[520px] bg-purple rounded-xl py-10 mx-auto">
         <div className="flex max-w-sm mx-auto flex-wrap items-center flex-col justify-center">
-          <h2 className="text-white sm:text-4xl text-3xl font-bold text-center my-auto pb-10">Play and learn with <br />your friends now!</h2>
+          <h2 className="text-white sm:text-4xl text-3xl font-bold text-center my-auto pb-10">Play and learn with <span className="block">your friends now!</span></h2>
           <Image src={friendsHero} alt="Friends hero" className="pb-10" height={230} />
           <Button>FIND FRIENDS</Button>
         </div>
       </div>
       <div className="w-full max-w-[520px] bg-green rounded-xl py-10 mx-auto">
         <div className="flex max-w-sm mx-auto flex-wrap items-center flex-col justify-center">
-          <h2 className="text-white sm:text-4xl text-3xl font-bold text-center my-auto pb-10">Daily quiz <br />Learn new things!</h2>
+          <h2 className="text-white sm:text-4xl text-3xl font-bold text-center my-auto pb-10">Daily quiz <span className="block">Learn new things!</span></h2>
           <Image src={brainStorm} alt="Friends hero" className="pb-10" height={230} />
           <Button>GO QUIZ ME</Button>
         </div>
