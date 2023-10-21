57728ab01665b7cb3cacbf58e2877d60555f2b56
Bartłomiej <bartlomiej.ostojski@teb.edu.pl>
2023-10-21 13:19:43 +0200

    feat: Add button animation on hover

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 const Button = ({ children } : { children : React.ReactNode}) => {
   return (
-    <button className="bg-black text-white py-4 sm:px-24 w-11/12 sm:w-fit rounded-full font-bold">
+    <button className="bg-black text-white py-4sm:px- 24 w-11/12 sm:w-fit rounded-full font-bold hover:text-gray-300">
       {children}
     </button>
   )
