052fa9074af423ad9082f1408bd4241b0d9bb689
Bartłomiej <bartlomiej.ostojski@teb.edu.pl>
2023-10-25 20:09:16 +0200

    feat: Add useLocalStorage hook

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,55 @@
+"use client";
+import { useState, useEffect } from 'react';
+
+const useLocalStorage = (key : string, initialValue : string) => {
+  const [value, setValue] = useState(() => { 
+    if (typeof window === 'undefined') {
+      return initialValue;
+    }
+
+    try {
+      const item = localStorage.getItem(key);
+      if(!item){
+        localStorage.setItem(key, JSON.stringify(initialValue));
+        return initialValue;
+      }else{
+        return JSON.parse(item);
+      }
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  const setLocalStorage = (newValue : string) => {
+    try {
+      localStorage.setItem(key, JSON.stringify(newValue));
+      setValue(newValue);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    const handleStorageChange = (e: StorageEvent) => {
+      if (e.key === key) {
+        try {
+          const newValue = JSON.parse(e.newValue || '');
+          setValue(newValue);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  }, [key]);
+
+  return [value, setLocalStorage]
+}
+
+export default useLocalStorage;
