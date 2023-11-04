54eb17600fb804fd38c3738430ea485f8a1b3b04
Bartłomiej <bartlomiej.ostojski@teb.edu.pl>
2023-11-04 18:31:47 +0100

    feat: Add mongodb

---
+++ src/utils/database.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
+
+  if(isConnected) {
+    return;
+  }
+  
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = true;
+  } catch (error) {
+    console.log(error);
+  }
+}
