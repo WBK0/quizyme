7351a2f697171992cdf6a91b5b4fba7b56119a81
Bartłomiej <bartlomiej.ostojski@teb.edu.pl>
2023-11-14 20:45:57 +0100

    feat: Add complete register callback

---
+++ src/app/(auth)/auth/complete-register/callback/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import Image from "next/image";
+import logo from '@/public/logo.svg';
+import { useRouter } from "next/navigation";
+
+const Callback = () => {
+  const { update, status, data } = useSession();
+  const router = useRouter();
+
+  const refreshToken = async () => {
+    if(data){
+      setTimeout(async () => {
+        await update();
+        if(data?.user?.isComplete){
+          router.replace('/');
+        }
+      }, 500);
+    }
+  }
+
+  useEffect(() => {
+    refreshToken();
+  }, [status]);
+
+  return (
+    <div className="max-w-sm mx-auto flex h-screen flex-col justify-center px-3 gap-4 items-center">
+      <div className="animate-bounce">
+        <Image src={logo} width={150} height={150} alt="logo" 
+          className="my-4"
+        />
+      </div>      
+      <h1 className="font-black text-4xl text-center">Welcome in our family!</h1>
+      <p className="text-center text-sm mt-6">
+        Please wait while we are saving all your data and configuration your account!
+      </p>
+      <p className="text-center text-sm ">
+        You will be redirected to the homepage in a few seconds...
+      </p>
+    </div>
+  )
+}
+export default Callback;
