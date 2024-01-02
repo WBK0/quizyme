8e994d67fe1253828879378cc03913f01faf5b26
Bartłomiej <bartlomiej.ostojski@teb.edu.pl>
2024-01-02 15:32:21 +0100

    feat: Add quiz game start route

--- src/app/(app)/play/quiz/start/page.tsx
+++ src/app/(app)/play/quiz/start/page.tsx
@@ -2,8 +2,8 @@
 
 const StartQuiz = () => {
     
-  const handleStart = async () => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API}/play/quiz/start`, {
+  const handleCreate = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API}/play/quiz/create`, {
       method: 'POST',
       body: JSON.stringify({
         quizId: '658db2c923a2d55edb5a01d2',
@@ -17,11 +17,32 @@ const StartQuiz = () => {
     console.log(await response.json());
   }
 
-  return (
+  const handleStart = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API}/play/quiz/65941baf63f72165bd5a1d65/start`, {
+      method: 'POST',
+      body: JSON.stringify({
+        quizId: '658db2c923a2d55edb5a01d2',
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      cache: 'no-cache',
+    });
+
+    console.log(await response.json());
+  }
+
+  return (<>
+  <button
+      type="button"
+      onClick={handleCreate}
+    >create</button>
     <button
       type="button"
       onClick={handleStart}
-    >page</button>
+    >start</button>
+  </>
+    
   )
 }
 export default StartQuiz;
---
+++ src/app/api/play/quiz/[id]/start/route.ts
@@ -0,0 +1,106 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import checkMongoDBID from "@/utils/checkMongodbID";
+import { connectToDB } from "@/utils/database";
+import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { NextRequest } from "next/server";
+
+export const POST = async (req: NextRequest, { params } : {params : {id: string}}) => {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if(!session?.user?.id) {
+      return new Response(
+        JSON.stringify({
+          status: "Error",
+          message: "User not logged in",
+        }),
+        { status: 401 }
+      );
+    }
+
+    const { id } = params;
+
+    if(!checkMongoDBID(id)){
+      return new Response(
+        JSON.stringify({
+          status: "Error",
+          message: "Invalid format of quiz id provided",
+        }),
+        { status: 400 }
+      );
+    }
+
+    connectToDB();
+    const prisma = new PrismaClient();
+
+    const quizGame = await prisma.quizGame.findUnique({
+      where: {
+        id: id
+      },
+      include: {
+        quiz: true,
+      }
+    });
+
+    if(!quizGame) {
+      return new Response(
+        JSON.stringify({
+          status: "Error",
+          message: "Invalid quiz game id provided",
+        }),
+        { status: 400 }
+      );
+    }
+
+    if(quizGame.isStarted) {
+      return new Response(
+        JSON.stringify({
+          status: "Error",
+          message: "Quiz game already started",
+        }),
+        { status: 400 }
+      );
+    }
+
+    const timeForQuestion = quizGame.quiz.questions[quizGame?.questionsOrder[quizGame.actualQuestion]].time;
+
+    const timeToRespond = new Date(Date.now() + timeForQuestion * 1000 + 5000) // 5 seconds for safety reasons
+
+    const updateQuizGame = await prisma.quizGame.update({
+      where: {
+        id: id
+      },
+      data: {
+        isStarted: true,
+        timeToRespond: timeToRespond,
+      }
+    });
+
+    if(!updateQuizGame) {
+      return new Response(
+        JSON.stringify({
+          status: "Error",
+          message: "Failed to start quiz game",
+        }),
+        { status: 400 }
+      );
+    }
+
+    return new Response(
+      JSON.stringify({
+        status: "Success",
+        message: "Quiz game started successfully",
+      }),
+      { status: 200 }
+    )
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        status: "Error",
+        message: "Something went wrong",
+      }),
+      { status: 500 }
+    )
+  }
+}
