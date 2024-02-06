e99afecadae488e1bd27434e491e5d7830d3edc1
Bartłomiej <bartlomiej.ostojski@teb.edu.pl>
2024-02-06 16:37:59 +0100

    feat: Add animation on change card

--- src/app/(play)/play/flashcards/[id]/Playground/Playground.tsx
+++ src/app/(play)/play/flashcards/[id]/Playground/Playground.tsx
@@ -22,22 +22,54 @@ const Playground = () => {
   const cardRef = useRef<HTMLDivElement>(null);
   const conceptRef = useRef<HTMLDivElement>(null);
   const definitionRef = useRef<HTMLDivElement>(null);
+  const [animate, setAnimate] = useState<'left' | 'right' | null>(null);
+  const animateRef = useRef<HTMLDivElement>(null);
+
+  const handleAnimate = (direction: 'left' | 'right') => {
+    setAnimate(direction);
+
+    setTimeout(() => {
+      animateRef.current?.classList.add('scale-105');
+    }, 0)
+    setTimeout(() => {
+      if(direction === 'left') {
+        animateRef.current?.classList.add('left-[-10rem]')
+      }else{
+        animateRef.current?.classList.add('left-40')
+      }
+      animateRef.current?.classList.remove('duration-300')
+      animateRef.current?.classList.add('duration-500')
+    }, 300)
+    setTimeout(() => {
+      animateRef.current?.classList.add('opacity-0')
+    }, 300)
+    setTimeout(() => {
+      setAnimate(null);
+    }, 550)
+  }
 
   const calculateHeight = () => {
     if(!cardRef.current || !conceptRef.current || !definitionRef.current || !window) return;
 
     cardRef.current.style.height = 
       Number(conceptRef.current?.offsetHeight) > Number(definitionRef.current?.offsetHeight) 
-      ? Number(conceptRef.current?.offsetHeight) > Number(cardRef.current?.offsetWidth) * 0.5625
+      ? Number(conceptRef.current?.offsetHeight) > Number(cardRef.current?.offsetWidth) * (window.innerWidth < 640 ? 1 : 0.5625)
         ? conceptRef.current?.offsetHeight + 'px'
         : window.innerWidth < 640
           ? Number(cardRef.current?.offsetWidth) + "px"
           : Number(cardRef.current?.offsetWidth) * 0.5625 + "px"
-      : Number(definitionRef.current?.offsetHeight) > Number(cardRef.current?.offsetWidth) * 0.5625
+      : Number(definitionRef.current?.offsetHeight) > Number(cardRef.current?.offsetWidth) * (window.innerWidth < 640 ? 1 : 0.5625)
         ? definitionRef.current?.offsetHeight + 'px'
         : window.innerWidth < 640
           ? Number(cardRef.current?.offsetWidth) + "px"
           : Number(cardRef.current?.offsetWidth) * 0.5625 + "px"
+
+      if(animateRef.current){
+        if(animateRef.current.clientHeight < cardRef.current.clientHeight) {
+          animateRef.current.style.height = cardRef.current.style.height;
+          animateRef.current.style.width = cardRef.current.style.width;
+        }
+      }
   }
 
   const handleShowing = () => {
@@ -50,10 +82,12 @@ const Playground = () => {
     if(method === 'increase') {
       if(card < list.length - 1) {
         setCard(card + 1)
+        handleAnimate('left');
       }
     } else {
       if(card > 0) {
         setCard(card - 1)
+        handleAnimate('right');
       }
     }
   }
@@ -74,6 +108,32 @@ const Playground = () => {
 
   return (
     <>
+      {
+        animate ?
+          <div 
+            className='w-full absolute left-0 top-0 z-50 duration-300' 
+            ref={animateRef}
+            style={{
+              height: cardRef.current?.style.height,
+              width: cardRef.current?.clientWidth + 'px'
+            }}
+          >
+            <div 
+              className="bg-green rounded-2xl cursor-pointer flip-card-inner relative w-full h-full flex items-center justify-center" 
+            >
+              <p 
+                className="font-bold text-white text-lg md:text-2xl text-center py-5 px-3"
+              >
+                {
+                  animate === 'left' 
+                    ? list[card - 1][cardRef.current?.classList.contains('rotate') ? 'definition' : 'concept']
+                    : list[card + 1][cardRef.current?.classList.contains('rotate') ? 'definition' : 'concept']
+                }
+              </p>
+            </div>
+          </div>
+        : null
+      }
       <div 
         className='flip-card bg-transparent w-full aspect-video' 
         onClick={handleShowing}
